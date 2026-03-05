22fb8ac97963daaee8be5e1b7210024f8a06d553
Rizky Yogo Prayogi <rizky.prayogi94@gmail.com>
2026-03-05 23:42:22 +0800

    fix flex table pelanggaran

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -27,13 +27,13 @@ const Layout = () => {
     }
 
     return (
-        <div className="min-h-screen flex bg-gray-50">
+        <div className="min-h-screen flex bg-gray-50 overflow-hidden">
             <Sidebar
                 isOpen={sidebarOpen}
                 onClose={() => setSidebarOpen(false)}
             />
 
-            <div className="flex-1 flex flex-col min-h-screen">
+            <div className="flex-1 flex flex-col min-h-screen min-w-0">
                 <Navbar onMenuClick={() => setSidebarOpen(true)} />
 
                 <main className="flex-1 p-4 lg:p-6 overflow-auto">
